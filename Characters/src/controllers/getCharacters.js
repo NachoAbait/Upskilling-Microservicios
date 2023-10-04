5bbc9ce3a1f2cea9dc12a2224c8cda01c1c3b74e
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-10-04 10:56:12 -0300

    saque el actualizado

--- Characters/src/controllers/getCharacters.js
+++ Characters/src/controllers/getCharacters.js
@@ -3,6 +3,5 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
   const characters = (await axios.get("http://database:8004/Characters")).data;
-  console.log("actualizado");
   res.status(200).json(characters);
 };
