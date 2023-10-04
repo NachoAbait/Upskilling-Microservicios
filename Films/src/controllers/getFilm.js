9be202b4c2d84ba419391b12415293a1270a9874
Nacho Abait <nachoabait@hotmail.com>
2023-10-04 12:27:47 -0300

    Update getFilm.js
    s

--- Films/src/controllers/getFilm.js
+++ Films/src/controllers/getFilm.js
@@ -4,5 +4,5 @@ module.exports = async (req, res) => {
   const { id } = req.params;
   const film = (await axios.get(`http://database:8004/Films/${id}`)).data;
 
-  res.status(200).json(films);
+  res.status(200).json(film);
 };
