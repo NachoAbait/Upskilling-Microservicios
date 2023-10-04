01839802b3b2530742d371ad12755f0f2b32daf8
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-10-04 11:35:50 -0300

    a ver ahora

--- Characters/src/controllers/getCharacter.js
+++ Characters/src/controllers/getCharacter.js
@@ -3,7 +3,8 @@ const Characters = require("../data");
 const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const character = (await axios.get("http://database:8004/Characters/:id"))
+  const { id } = req.params;
+  const character = (await axios.get(`http://database:8004/Characters/${id}`))
     .data;
   res.status(200).json(character);
 };
